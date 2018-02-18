7e0dc4b72794080ae5769684004452517afb47ad
Andrew Hwang <andrewhwang@Andrews-MacBook-Pro.local>
2018-02-17 21:23:22 -0500

    made navigation bar for each tab.

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React from 'react';
+import { Tabs } from './src/components/router.js';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Tabs />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/dashboard/Videos/Videos.js
+++ src/dashboard/Videos/Videos.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default class Videos extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Videos!</Text>
+        <ScrollView>
+          <FlatList
+              data={[
+                // {key: 'Aerospace Engineering'},
+                // {key: 'Astronomy'},
+                // {key: 'Biochemistry'},
+                // {key: 'Biology'},
+                // {key: 'Chemical Engineering'},
+                // {key: 'Civil Engineering'},
+                {key: 'Software Engineering'},
+                // {key: 'Electrical Engineering'},
+                // {key: 'Mathematics'},
+                // {key: 'Mechanical Engineering'},
+                // {key: 'Physics'},
+                // {key: 'Psychology'},
+                // {key: 'Statistics'},
+              ]}
+              renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+            />
+          </ScrollView>
       </View>
     );
   }
@@ -13,9 +32,12 @@ export default class Videos extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
